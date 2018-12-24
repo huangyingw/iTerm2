ceed6f69f26a4d8e650ec23c265b42ecbeed1ed0
Ying Huang <yhuang@imvu.com>
2018-12-24 14:14:41 -0800

    n

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 xcodebuild -list -project iTerm2.xcodeproj
+xcodebuild -target iTerm2Shared
+xcodebuild -target SSKeychain
 xcodebuild
+open /Users/huangyingw/Dropbox/myproject/git/mac/gnachman/iTerm2/build/Development/iTerm2.app
