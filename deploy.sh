81402cb1b0b5d80828c98d53013b2d05fc234faf
Ying Huang <huangyingw@gmail.com>
2021-03-31 21:47:11 -0700

    n

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-~/loadrc/bashrc/ln_fs.sh ~/loadrc/iterm2rc/movescreen.py ~/Library/ApplicationSupport/iTerm2/Scripts/movescreen/movescreen/movescreen.py
+cp -fv ./movescreen.py ~/Library/ApplicationSupport/iTerm2/Scripts/movescreen/movescreen/movescreen.py
